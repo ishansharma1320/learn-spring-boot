857d6eecd25e85e81ec3cd50d25639e80a2241e9
Ishan Sharma <ishansharma1320@gmail.com>
2023-05-06 14:55:22 -0300

    Setup Base Project

---
+++ src/main/java/com/ishanlearnsspringboot/springsection2/SpringSection2Application.java
@@ -0,0 +1,13 @@
+package com.ishanlearnsspringboot.springsection2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSection2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSection2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ishanlearnsspringboot/springsection2/SpringSection2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishanlearnsspringboot.springsection2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSection2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
