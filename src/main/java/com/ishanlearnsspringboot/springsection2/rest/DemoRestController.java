8d3ce5c9df8137017bc52d7bf75cfda22fdb4802
Ishan Sharma <ishansharma1320@gmail.com>
2023-05-07 13:01:16 -0300

    Performed Setter Injection

--- src/main/java/com/ishanlearnsspringboot/springsection2/rest/DemoRestController.java
+++ src/main/java/com/ishanlearnsspringboot/springsection2/rest/DemoRestController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoRestController {
     private Coach myCoach;
     @Autowired
-    DemoRestController(Coach myCoach){
-        this.myCoach = myCoach;
+    private void setCoach(Coach coach){
+        this.myCoach = coach;
     }
 
     @GetMapping("/getDailyWorkout")
