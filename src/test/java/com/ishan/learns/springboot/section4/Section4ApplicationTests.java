44d0b40cfa3eee146f6c07d72ab333226b5e342a
Ishan Sharma <ishansharma1320@gmail.com>
2023-05-09 21:19:24 -0300

    Added Initial Code for section-4

--- src/test/java/com/ishan/learns/springboot/section4/Section4ApplicationTests.java
+++ src/test/java/com/ishan/learns/springboot/section4/Section4ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ishan.learns.springboot.hibernatedemo;
+package com.ishan.learns.springboot.section4;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HibernateDemoApplicationTests {
+class Section4ApplicationTests {
 
 	@Test
 	void contextLoads() {
