0cd255dc067f6db3bb3b3838e9274c00ec8a162c
Ishan Sharma <ishansharma1320@gmail.com>
2023-05-14 11:55:31 -0300

    Added Exception Handling to Rest API in REST controller

---
+++ src/main/java/com/ishan/learns/springboot/section4/rest/StudentErrorResponse.java
@@ -0,0 +1,42 @@
+package com.ishan.learns.springboot.section4.rest;
+
+public class StudentErrorResponse {
+
+    private int status;
+    private String message;
+    private long timestamp;
+
+    StudentErrorResponse(){
+
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public StudentErrorResponse(int status, String message, long timestamp) {
+        this.status = status;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+}
--- src/main/java/com/ishan/learns/springboot/section4/rest/StudentRestController.java
+++ src/main/java/com/ishan/learns/springboot/section4/rest/StudentRestController.java
@@ -1,9 +1,10 @@
 package com.ishan.learns.springboot.section4.rest;
 
 import com.ishan.learns.springboot.section4.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +13,49 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("students")
-    private List<Student> getStudents(){
-        List<Student> students = new ArrayList<>();
+    private List<Student> students;
+    @PostConstruct
+    private void loadData(){
+        students = new ArrayList<>();
         students.add(new Student("Ishan","Sharma"));
         students.add(new Student("John","Doe"));
         students.add(new Student("James","Bond"));
-
+    }
+    @GetMapping("/students")
+    private List<Student> getStudents(){
         return students;
+    }
+
+
+    @GetMapping("/students/{studentId}")
+    private Student getStudentById(@PathVariable int studentId){
+        int actualStudentId = studentId - 1; // because studentId starts from 1
+        Student result = null;
+        if (actualStudentId >= 0 && actualStudentId < this.students.size()){
+            result = this.students.get(actualStudentId);
+        } else {
+            throw new StudentNotFoundException("student not found with ID: "+studentId);
+        }
+        return result;
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<StudentErrorResponse> handleStudentNotFoundException(StudentNotFoundException exc){
+        StudentErrorResponse errorResponse = new StudentErrorResponse();
+        errorResponse.setMessage(exc.getMessage());
+        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        errorResponse.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(errorResponse,HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<StudentErrorResponse> handleAllExceptions(Exception exc){
+        StudentErrorResponse errorResponse = new StudentErrorResponse();
+        errorResponse.setMessage(exc.getMessage());
+        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorResponse.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(errorResponse,HttpStatus.BAD_REQUEST);
     }
 }
