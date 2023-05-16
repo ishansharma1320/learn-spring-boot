073bd5ef2997936464cd4482c420fbf3bf66fb3f
Ishan Sharma <ishansharma1320@gmail.com>
2023-05-16 10:44:52 -0300

    Created a Spring Project for crud Demo

--- src/main/java/com/ishan/learns/springboot/cruddemo/CrudDemoApplication.java
+++ src/main/java/com/ishan/learns/springboot/cruddemo/CrudDemoApplication.java
@@ -1,13 +1,13 @@
-package com.ishan.learns.springboot.section4;
+package com.ishan.learns.springboot.cruddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Section4Application {
+public class CrudDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Section4Application.class, args);
+		SpringApplication.run(CrudDemoApplication.class, args);
 	}
 
 }
--- src/test/java/com/ishan/learns/springboot/cruddemo/CrudDemoApplicationTests.java
+++ src/test/java/com/ishan/learns/springboot/cruddemo/CrudDemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ishan.learns.springboot.section4;
+package com.ishan.learns.springboot.cruddemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Section4ApplicationTests {
+class CrudDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
