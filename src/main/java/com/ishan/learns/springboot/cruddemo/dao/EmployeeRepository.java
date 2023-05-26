d1d9342d1c3d1bb9bbc136120913781e8553adb6
Ishan Sharma <ishansharma1320@gmail.com>
2023-05-25 22:22:17 -0300

    Using Spring Data Rest - Now Section - 4 Complete

--- src/main/java/com/ishan/learns/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/ishan/learns/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,6 +2,9 @@ package com.ishan.learns.springboot.cruddemo.dao;
 
 import com.ishan.learns.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+@RepositoryRestResource(path ="members")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 }
